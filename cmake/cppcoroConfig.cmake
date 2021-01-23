2f54023f5148ac13bb825c61fc568cb26a61978b
Alexander Karzhenkov <karzhenkov@mail.ru>
2021-01-23 16:30:48 +0500

    Properly configure installed Coroutines module

--- cmake/cppcoroConfig.cmake
+++ cmake/cppcoroConfig.cmake
@@ -1,6 +1,6 @@
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
 include(CMakeFindDependencyMacro)
-find_dependency(Coroutines QUIET REQUIRED)
+find_dependency(Coroutines QUIET REQUIRED COMPONENTS @CXX_COROUTINES_COMPONENT@)
 
 include("${CMAKE_CURRENT_LIST_DIR}/cppcoroTargets.cmake")
